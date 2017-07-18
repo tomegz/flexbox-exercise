7282709867d60b89208cc1fa8915f304b2c070b9
tomegz <tomaszbubala@o2.pl>
2017-07-18 09:29:28 +0200

    finish flexbox form

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require("gulp");
 var autoprefixer = require("gulp-autoprefixer");
 
 gulp.task("styles", function() {
-  gulp.src("css/styles.css")
+  gulp.src("css/style6.css")
     .pipe(autoprefixer())
     .pipe(gulp.dest("build"))
 });
 
 gulp.task("watch", function() {
-  gulp.watch("css/styles.css", ["styles"]);
+  gulp.watch("css/styles6.css", ["styles"]);
 })
